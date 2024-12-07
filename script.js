cfe35c39a7144c0dab5ee41ff0289fc2fa672540
ZayanKamran <ziyanffking@gmail.com>
2024-12-07 17:48:30 +0500

    Product Carts

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Function to show slides
+
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+        slides[i].style.display = "none";
     }
-    slides[slideIndex-1].style.display = "block";  
+    slides[slideIndex - 1].style.display = "block";
 }
 
-// Auto-scroll every 5 seconds
-setInterval(function() {
+
+setInterval(function () {
     slideIndex++;
     showSlides(slideIndex);
-}, 5000); // 5000 milliseconds = 5 seconds
+}, 5000); 
